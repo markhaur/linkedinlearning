b263fe1af6c22818bd549b43983389f55409a361
Faisal Nisar <fnisar394@protonmail.com>
2022-08-17 12:02:15 +0500

    completed challenge fizzbuzz

---
+++ go-essential-training/go-basics/challenge-fizzbuzz/challenge.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 20; i++ {
+
+		if i%3 == 0 {
+			fmt.Print("fizz ")
+		}
+
+		if i%5 == 0 {
+			fmt.Print("buzz")
+		}
+
+		if i%3 != 0 && i%5 != 0 {
+			fmt.Print(i)
+		}
+
+		fmt.Println("")
+	}
+}
