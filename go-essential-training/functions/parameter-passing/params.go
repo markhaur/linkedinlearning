99b7d02760d2a0ed46090480f04d9e8cf858a8b4
Faisal Nisar <fnisar394@protonmail.com>
2022-08-20 17:44:57 +0500

    completed lecture parameter passing

---
+++ go-essential-training/functions/parameter-passing/params.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	values := []int{1, 2, 3, 4}
+	doubleAt(values, 2)
+	fmt.Println(values)
+
+	val := 10
+	double(val)
+	fmt.Println(val)
+
+	doublePtr(&val)
+	fmt.Println(val)
+}
+
+func doubleAt(vals []int, i int) {
+	vals[i] *= 2
+}
+
+func double(n int) {
+	n *= 2
+}
+
+func doublePtr(n *int) {
+	*n *= 2
+}
