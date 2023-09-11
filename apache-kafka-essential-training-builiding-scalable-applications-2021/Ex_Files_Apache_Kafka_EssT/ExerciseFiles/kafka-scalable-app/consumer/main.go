d179ac7cdbed5a7da1e0ea0628151049dd14d324
Faisal Nisar <fnisar394@protonmail.com>
2023-09-11 10:02:01 +0500

    added consumer client implementation for kafka

--- apache-kafka-essential-training-builiding-scalable-applications-2021/Ex_Files_Apache_Kafka_EssT/ExerciseFiles/kafka-scalable-app/consumer/main.go
+++ apache-kafka-essential-training-builiding-scalable-applications-2021/Ex_Files_Apache_Kafka_EssT/ExerciseFiles/kafka-scalable-app/consumer/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/segmentio/kafka-go"
+)
+
+const (
+	topic       = "kafka.learning.orders"
+	broker1Addr = "localhost:9092"
+	broker2Addr = "localhost:9093"
+	broker3Addr = "localhost:9094"
+)
+
+func main() {
+	ctx := context.Background()
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{broker1Addr, broker2Addr, broker3Addr},
+		Topic:   topic,
+		GroupID: "kafka-go-consumer",
+	})
+
+	for i := 0; i <= 20; i++ {
+		msg, err := reader.ReadMessage(ctx)
+		if err != nil {
+			fmt.Printf("error while reading message: %v\n", err)
+			break
+		}
+
+		fmt.Printf("received: %s\n", string(msg.Value))
+	}
 }
